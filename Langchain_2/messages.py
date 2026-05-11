9c68e206af9204765c78da94ed0d16f381cfc9b4
Shraddha Tiwari <shraddhatiwari6211@gmail.com>
2026-05-11 12:30:32 +0545

    Practice 3 types of messages: Way to storechat history

---
+++ Langchain_2/messages.py
@@ -0,0 +1,18 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+messages = [
+    SystemMessage(content= 'Your are a helpful assistant'),
+    HumanMessage(content= 'Tell me about Langchain')
+]
+
+result = model.invoke(messages)
+
+messages.append(AIMessage(content=result.content))
+
+print(messages)
